fae686f5af2c5e8ca8697e3ce97232430124cf9c
sachin38 <sachin.adhikari38@gmail.com>
2017-05-04 22:56:47 +0545

    rbl format

--- new_nepse.py
+++ new_nepse.py
@@ -3,13 +3,13 @@ import requests
 import csv
 import os.path
 
-def fetchdata():
-    symbol = raw_input("Enter the stock symbol: ")
+def fetchdata(symbols):
+    #symbol = raw_input("Enter the stock symbol: ")
     url = "http://www.nepalstock.com/company/"
 
     try:
         #make post requests with that symbol and capture it in req 
-        req = requests.post(url, data={"stock_symbol":symbol}, verify=False)
+        req = requests.post(url, data={"stock_symbol":symbols}, verify=False)
     except requests.exceptions.RequestException as e:
         print e
         sys.exit(1)
@@ -19,12 +19,13 @@ def fetchdata():
     datas = []
     company_name = table.findAll("td")[0].string
     datas.append(company_name)
+    print company_name
     for row in table.findAll("tr")[7:]:
         col = row.findAll("td")
         print col[0].string,": ",col[1].string
         datas.append(col[1].string)
-   
-    write_to_csv(datas)
+        
+    #write_to_csv(datas)
     datas[:] = []
 
 def write_to_csv(received_data = [], *args):
@@ -42,5 +43,10 @@ def write_to_csv(received_data = [], *args):
             writer.writerow(received_data)
             received_data[:] = []
 
-
-fetchdata()
+line_list = []
+with open("symbols.txt", "r") as filet:
+    for line in filet:
+        line_list.append(line)
+for text in line_list:
+    print type(text)
+    fetchdata(text)
